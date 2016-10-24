69003e94aa794589fefad7f6669c960a76e1cf58
Ramil Yabbarov <ram0973@gmail.com>
2016-10-24 11:14:05 +0300

    добавил docstrings

--- password_strength.py
+++ password_strength.py
@@ -15,14 +15,20 @@ def enable_win_unicode_console():
 def password_strength(password: str) -> int:
     """
     Вычисляем стойкость пароля
-    :param password:
+    :param password: текстовый пароль
     :return: стойкость пароля в баллах от 1 до 10
     """
+    # список баллов нужен из-за того, что функция от zxcvbn возвращает
+    # стойкость пароля в баллах от 0 до 4, а нам нужно от 1 до 10
     password_scores = [1, 3, 5, 7, 10]
     return password_scores[zxcvbn_password_strength(password)['score']]
 
 
 def print_password_strength_attributes(password: str):
+    """
+    Печатаем параметры пароля - энтропия, время взлома
+    :param password: текстовый пароль
+    """
     password_strength_attributes = zxcvbn_password_strength(password)
     print('Энтропия пароля, бит: %s' % password_strength_attributes['entropy'])
     print('Ожидаемое время взлома, сек: %s (%s)' %
