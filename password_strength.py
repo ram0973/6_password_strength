2f8f573455cc84e5823858cbfd82a11198891694
Ramil Yabbarov <ram0973@gmail.com>
2016-11-16 16:12:08 +0300

    исправил ошибки

--- password_strength.py
+++ password_strength.py
@@ -1,28 +1,17 @@
 # -*- coding: utf-8 -*-
 from getpass import getpass
 import sys
-from zxcvbn import password_strength as zxcvbn_password_strength
+from zxcvbn import password_strength
 
 
-def enable_win_unicode_console():
-    """
-    Включаем правильное отображение unicode в консоли под MS Windows
-    """
-    if sys.platform == 'win32':
-        import win_unicode_console
-        win_unicode_console.enable()
-
-
-def password_strength(password: str) -> int:
+def get_password_strength(password: str) -> int:
     """
     Вычисляем стойкость пароля
     :param password: текстовый пароль
     :return: стойкость пароля в баллах от 1 до 10
     """
-    # список баллов нужен из-за того, что функция от zxcvbn возвращает
-    # стойкость пароля в баллах от 0 до 4, а нам нужно от 1 до 10
-    password_scores = [1, 3, 5, 7, 10]
-    return password_scores[zxcvbn_password_strength(password)['score']]
+    score = password_strength(password)['score']
+    return (score + 1) * 2 if score != 0 else 1
 
 
 def print_password_strength_attributes(password: str):
@@ -30,19 +19,18 @@ def print_password_strength_attributes(password: str):
     Печатаем параметры пароля - энтропия, время взлома
     :param password: текстовый пароль
     """
-    password_strength_attributes = zxcvbn_password_strength(password)
+    password_strength_attributes = password_strength(password)
     print('Энтропия пароля, бит: %s' % password_strength_attributes['entropy'])
     print('Ожидаемое время взлома, сек: %s (%s)' %
           (password_strength_attributes['crack_time'],
            password_strength_attributes['crack_time_display'])
           )
     print('Стойкость пароля по шкале от 1 до 10: %s' %
-          password_strength(password))
+          get_password_strength(password))
 
 
 if __name__ == '__main__':
 
-    enable_win_unicode_console()
     user_password = getpass('Введите пароль для проверки: ').strip()
     print_password_strength_attributes(user_password)
 
